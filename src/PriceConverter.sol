d58c36b6061656f4ec35c25f7f31974347d7c6a5
pratokko <pratokko3@gmail.com>
2023-08-22 01:59:52 +0300

    feat: priceConverter-implementation

---
+++ src/PriceConverter.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+
+        (, int price, , , ) = priceFeed.latestRoundData();
+
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() internal view returns (uint256) {
+        return
+            AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306)
+                .version();
+    }
+}
