98174d6a7b268f984153fa5bcc36ede641fbbaca
pratokko <pratokko3@gmail.com>
2023-08-22 01:45:14 +0300

    feat: withdraw implementation

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
     uint256 public constant MINIMUM_USD = 5e18;
@@ -12,5 +13,23 @@ contract FundMe {
         require(msg.value >= MINIMUM_USD, "You have not sent enough");
         s_funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < s_funders.length;
+            s_funders++
+        ) {
+            address funder = s_funders[funderIndex];
+
+            addressToAmountFunded[funder] = 0;
+        }
+
+        s_funders = new address[](0);
+
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+
+        require(success, "Transfer Failed");
     }
 }
