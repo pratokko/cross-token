c95af0c66111f792e2e25154d86c755c85ceae71
pratokko <pratokko3@gmail.com>
2023-08-22 01:07:26 +0300

    feat: fund-function

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -2,4 +2,15 @@
 
 pragma solidity ^0.8.18;
 
-contract FundMe {}
+contract FundMe {
+    uint256 public constant MINIMUM_USD = 5e18;
+
+    address[] private s_funders;
+    mapping(address funder => uint256 amount) public addressToAmountFunded;
+
+    function fund() public payable {
+        require(msg.value >= MINIMUM_USD, "You have not sent enough");
+        s_funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+}
