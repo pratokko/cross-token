7dc760de6be52c4ebd71d7fe4bca7210d6abda37
pratokko <pratokko3@gmail.com>
2023-08-22 04:15:20 +0300

    feat: scripts and tests

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 error FundMe__NotOwner();
@@ -9,25 +10,35 @@ contract FundMe {
     using PriceConverter for uint256;
     uint256 public constant MINIMUM_USD = 5e18;
 
-    address private immutable i_owner;
+    address public immutable i_owner;
+
+    AggregatorV3Interface private s_priceFeed;
 
     address[] private s_funders;
     mapping(address funder => uint256 amount) public addressToAmountFunded;
 
-    constructor() {
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+        _;
+    }
+
+    constructor(address priceFeed) {
         i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
     function fund() public payable {
         require(
-            msg.value.getConversionRate() >= MINIMUM_USD,
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
             "You have not sent enough"
         );
         s_funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public onlyOwner{
+    function withdraw() public onlyOwner {
         for (
             uint256 funderIndex = 0;
             funderIndex < s_funders.length;
@@ -45,10 +56,7 @@ contract FundMe {
         require(success, "Transfer Failed");
     }
 
-    modifier onlyOwner() {
-        if (msg.sender != i_owner) {
-            revert FundMe__NotOwner();
-        }
-        _;
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
     }
 }
