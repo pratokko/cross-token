9e076d1c168547d3aa4ab64a8d21d6b200d5e2fb
pratokko <pratokko3@gmail.com>
2023-08-24 16:44:54 +0300

    feat: interactions

--- test/unit/FundMeTest.t.sol
+++ test/unit/FundMeTest.t.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-import {FundMe} from "../src/FundMe.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
